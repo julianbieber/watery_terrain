db68a14db24eb14548e8ed0b07a86391ad368dc6
Julian Bieber <j.bieber@omniaretail.com>
2026-05-14 01:14:26 +0200

    start to incorporate sky plugin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 use bevy::{feathers::FeathersPlugins, prelude::*};
+use bevy_sky_gradient::{
+    aurora::{AuroraPlugin, AuroraSettings},
+    cycle::SkyCyclePlugin,
+    plugin::SkyPlugin,
+    prelude::{AmbientDriverPlugin, GradientDriverPlugin},
+    sun::{SunDriverPlugin, SunSettings},
+};
 
 use crate::{render::TerrainRanderPlugin, screens::ScreenPlugin, water_sim::WaterSimPlugin};
 
@@ -11,9 +18,33 @@ mod tooltip;
 mod water_sim;
 
 fn main() -> AppExit {
+    let sky_plugin = SkyPlugin::builder()
+        .set_sun_driver(SunDriverPlugin {
+            sun_settings: SunSettings {
+                sun_color: vec4(0.0, 0.0, 0.1, 1.0),
+                illuminance: 1.0,
+                sun_strength: 0.2,
+                sun_sharpness: 364.0,
+            },
+            spawn_default_sun_light: false,
+        })
+        .set_aurora(AuroraPlugin {
+            aurora_settings: AuroraSettings {
+                render_texture_percent: 0.25, // Render at 25% resolution
+                ..default()
+            },
+        })
+        .set_cycle(SkyCyclePlugin::default())
+        .set_gradient_driver(GradientDriverPlugin::default())
+        .set_ambient_driver(AmbientDriverPlugin {
+            ..Default::default()
+        })
+        .build();
+
     App::new()
         .add_plugins((
             DefaultPlugins,
+            sky_plugin,
             FeathersPlugins,
             ScreenPlugin,
             WaterSimPlugin,
