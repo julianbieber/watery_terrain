60a940e179cd4069dc1e9b0b7ead78fd132e89da
Julian Bieber <j.bieber@omniaretail.com>
2026-05-14 01:14:26 +0200

    start to incorporate sky plugin

--- src/gameplay/mod.rs
+++ src/gameplay/mod.rs
@@ -1,5 +1,3 @@
-use std::f32::consts::PI;
-
 use avian3d::{
     PhysicsPlugins,
     prelude::{Collider, Gravity, GravityScale, LinearVelocity, RigidBody},
@@ -7,12 +5,11 @@ use avian3d::{
 use bevy::{
     camera::Exposure,
     camera_controller::free_camera::{FreeCamera, FreeCameraPlugin},
-    feathers::palette::WHITE,
     image::ImageLoaderSettings,
-    light::CascadeShadowConfigBuilder,
     pbr::ExtendedMaterial,
     prelude::*,
 };
+use bevy_sky_gradient::plugin::SkyboxMagnetTag;
 
 use crate::{
     heightmap::create_heightmap,
@@ -31,10 +28,10 @@ impl Plugin for GameplayPlugin {
         app.add_plugins(FreeCameraPlugin);
         app.add_systems(OnEnter(Screen::Gameplay), spawn_plane_dbg);
         app.add_systems(Update, move_boat.run_if(in_state(Screen::Gameplay)));
-        app.add_systems(
-            Update,
-            pulse_ambient_light.run_if(in_state(Screen::Gameplay)),
-        );
+        // app.add_systems(
+        //     Update,
+        //     pulse_ambient_light.run_if(in_state(Screen::Gameplay)),
+        // );
     }
 }
 
@@ -58,6 +55,7 @@ fn spawn_player_camera(mut commands: Commands) {
             color: Color::Srgba(Srgba::RED),
             ..default()
         },
+        SkyboxMagnetTag,
     ));
 }
 
@@ -115,11 +113,11 @@ fn spawn_plane_dbg(
         })),
     ));
 
-    commands.insert_resource(GlobalAmbientLight {
-        color: WHITE.into(),
-        brightness: 50.0,
-        ..default()
-    });
+    // commands.insert_resource(GlobalAmbientLight {
+    //     color: WHITE.into(),
+    //     brightness: 50.0,
+    //     ..default()
+    // });
 
     commands.spawn((
         DespawnOnExit(Screen::Gameplay),
@@ -148,27 +146,27 @@ fn spawn_plane_dbg(
         MeshMaterial3d(standard_materials.add(Color::srgb_u8(24, 144, 255))),
     ));
 
-    commands.spawn((
-        DirectionalLight {
-            illuminance: light_consts::lux::OVERCAST_DAY,
-            shadows_enabled: true,
-            ..default()
-        },
-        Transform {
-            translation: Vec3::new(0.0, 2.0, 0.0),
-            rotation: Quat::from_rotation_x(-PI / 4.),
-            ..default()
-        },
-        // The default cascade config is designed to handle large scenes.
-        // As this example has a much smaller world, we can tighten the shadow
-        // bounds for better visual quality.
-        CascadeShadowConfigBuilder {
-            first_cascade_far_bound: 4.0,
-            maximum_distance: 10.0,
-            ..default()
-        }
-        .build(),
-    ));
+    // commands.spawn((
+    //     DirectionalLight {
+    //         illuminance: light_consts::lux::OVERCAST_DAY,
+    //         shadows_enabled: true,
+    //         ..default()
+    //     },
+    //     Transform {
+    //         translation: Vec3::new(0.0, 2.0, 0.0),
+    //         rotation: Quat::from_rotation_x(-PI / 4.),
+    //         ..default()
+    //     },
+    //     // The default cascade config is designed to handle large scenes.
+    //     // As this example has a much smaller world, we can tighten the shadow
+    //     // bounds for better visual quality.
+    //     CascadeShadowConfigBuilder {
+    //         first_cascade_far_bound: 4.0,
+    //         maximum_distance: 10.0,
+    //         ..default()
+    //     }
+    //     .build(),
+    // ));
 }
 
 fn move_boat(
@@ -182,15 +180,15 @@ fn move_boat(
             (time.elapsed_secs().cos() + ((time.elapsed_secs() * 0.3).sin().fract() * 2.0)) * 3.04;
     }
 }
-fn pulse_ambient_light(time: Res<Time>, mut ambient: ResMut<GlobalAmbientLight>) {
-    // --- tuneable constants ---
-    const MIN_BRIGHTNESS: f32 = 0.0;
-    const MAX_BRIGHTNESS: f32 = 100.0;
-    const CYCLE_SECS: f32 = 8.0; // full up-down-up period in seconds
+// fn pulse_ambient_light(time: Res<Time>, mut ambient: ResMut<GlobalAmbientLight>) {
+//     // --- tuneable constants ---
+//     const MIN_BRIGHTNESS: f32 = 0.0;
+//     const MAX_BRIGHTNESS: f32 = 100.0;
+//     const CYCLE_SECS: f32 = 8.0; // full up-down-up period in seconds
 
-    // sin oscillates in [-1, 1]; remap to [0, 1] then scale to [MIN, MAX]
-    let t = time.elapsed_secs();
-    let wave = (t * std::f32::consts::TAU / CYCLE_SECS).sin(); // -1 ..= 1
-    let normalized = (wave + 1.0) * 0.5; //  0 ..= 1
-    ambient.brightness = MIN_BRIGHTNESS + normalized * (MAX_BRIGHTNESS - MIN_BRIGHTNESS);
-}
+//     // sin oscillates in [-1, 1]; remap to [0, 1] then scale to [MIN, MAX]
+//     let t = time.elapsed_secs();
+//     let wave = (t * std::f32::consts::TAU / CYCLE_SECS).sin(); // -1 ..= 1
+//     let normalized = (wave + 1.0) * 0.5; //  0 ..= 1
+//     ambient.brightness = MIN_BRIGHTNESS + normalized * (MAX_BRIGHTNESS - MIN_BRIGHTNESS);
+// }
